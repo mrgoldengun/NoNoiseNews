e62f11af42d6a4f4735b5e33877312ce78f834f6
Vu Khanh Duy <vukhanhduy@192.168.73.49>
2025-09-07 10:40:00 +0700

    + Render news table from a given json data

---
+++ main.js
@@ -0,0 +1,68 @@
+
+const NEWS_TABLE_ID = "news-table"
+const NEWS_JSON_PATH = "./data/news.json"
+
+// ---------
+
+function renderNewsTable(newsJsonData)
+{
+    let writableNewsTableDivision = document.getElementById(NEWS_TABLE_ID);
+    let table = document.createElement("table");
+    table.setAttribute("border", "1");
+    table.setAttribute("cellpadding", "5");
+    table.setAttribute("cellspacing", "0");
+
+    let tableHead = document.createElement("thead");
+    let tableHeadRow = document.createElement("tr");
+    let headers = ["Title", "Author", "Category", "Publishing Time", "Source"];
+    for (let header of headers) {
+        let tableHeader = document.createElement("th");
+        
+        tableHeader.textContent = header;
+        tableHeadRow.appendChild(tableHeader);
+    }
+    tableHead.appendChild(tableHeadRow);
+
+    let tableBody = document.createElement("tbody");
+    for (let news of newsJsonData)
+    {
+        let tableBodyRow = document.createElement("tr");
+        let tableTitleData = document.createElement("td");
+        let tableAuthorData = document.createElement("td");
+        let tableCategoryData = document.createElement("td");
+        let tablePublishTimeData = document.createElement("td");
+        let tableSourceData = document.createElement("td");
+
+        let tableTitleAnchor = document.createElement("a");
+        tableTitleAnchor.textContent = news.title;
+        tableTitleAnchor.href = news.link;
+
+        tableTitleData.appendChild(tableTitleAnchor);
+        tableAuthorData.textContent = news.author;
+        tableCategoryData.textContent = news.category;
+        tablePublishTimeData.textContent = news.publishTime;
+        tableSourceData.textContent = news.source;
+
+        tableBodyRow.appendChild(tableTitleData);
+        tableBodyRow.appendChild(tableAuthorData);
+        tableBodyRow.appendChild(tableCategoryData);
+        tableBodyRow.appendChild(tablePublishTimeData);
+        tableBodyRow.appendChild(tableSourceData);
+
+        tableBody.appendChild(tableBodyRow);
+    }
+    
+    table.appendChild(tableHead);
+    table.appendChild(tableBody);
+
+    writableNewsTableDivision.appendChild(table)
+}
+
+window.onload = main();
+function main()
+{
+    fetch(NEWS_JSON_PATH)
+        .then(response => response.json())
+        .then(data => renderNewsTable(data))
+        .catch(error => console.error('Error loading news.json:', error));
+}
