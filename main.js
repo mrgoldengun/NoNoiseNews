3f4212fac75046dca2d935a886240f3f5d5cb207
Vu Khanh Duy <vukhanhduy@192.168.73.49>
2025-09-09 09:47:44 +0700

    + Added expandable section

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
-const NEWS_TABLE_ID = "news-table"
+const CRYPTO_NEWS_TABLE_ID = "crypto-news-table"
 const NEWS_JSON_PATH = "./data/news.json"
 
 // ---------
 
 function renderNewsTable(newsJsonData)
 {
-    let writableNewsTableDivision = document.getElementById(NEWS_TABLE_ID);
+    let writableNewsTableDivision = document.getElementById(CRYPTO_NEWS_TABLE_ID);
     let table = document.createElement("table");
     table.setAttribute("border", "1");
     table.setAttribute("cellpadding", "5");
@@ -15,7 +15,8 @@ function renderNewsTable(newsJsonData)
     let tableHead = document.createElement("thead");
     let tableHeadRow = document.createElement("tr");
     let headers = ["Title", "Author", "Category", "Publishing Time", "Source"];
-    for (let header of headers) {
+    for (let header of headers) 
+    {
         let tableHeader = document.createElement("th");
         
         tableHeader.textContent = header;
